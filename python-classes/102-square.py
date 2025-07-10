848e357b6b08780ba83b239f8f27e02d49fcae52
nshderrick056 <d.gatete@alustudent.com>
2025-07-10 20:44:56 +0200

    a class Square that defines a square by: (based on 4-square.py)
    Private instance attribute: size:
    property def size(self): to retrieve it
    property setter def size(self, value): to set it:
    size must be a number (float or integer), otherwise raise a TypeError exception with the message size must be a number
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with size: def __init__(self, size=0):
    Public instance method: def area(self): that returns the current square area
    Square instance can answer to comparators: ==, !=, >, >=, < and <= based on the square area
    You are not allowed to import any module

---
+++ python-classes/102-square.py
@@ -0,0 +1,42 @@
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area of the square."""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
