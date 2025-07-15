075ada63907f2471e442cb7af77fea8a9081d6ed
nshderrick056 <d.gatete@alustudent.com>
2025-07-15 16:13:24 +0200

    correcting the blank line at the end

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,7 +10,5 @@ class MyList(list):
     'print_sorted' that prints the list in ascending order.
     """
     def print_sorted(self):
-        """
-        Prints the list in ascending order without modifying the original list.
-        """
+        """ Prints the list in ascending order without modifying the original list."""
         print(sorted(self))
