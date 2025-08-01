4baa1d89fdafb4480104d280d91ee3fe89055fb4
nshderrick056 <d.gatete@alustudent.com>
2025-08-01 14:27:09 +0200

    correcting errors in 13 file

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """
-Script deletes all State objects with a name containing the letter a
-"""
+This script deletes all State objects from the database hbtn_0e_6_usa
+whose name contains the letter 'a'.
 
+Usage:
+    ./13-model_state_delete_a.py <mysql username> <mysql password> <database name>
+
+The script connects to a MySQL server running on localhost at port 3306
+using SQLAlchemy ORM. It imports the Base and State classes from model_state.
+"""
 
 import sys
 from model_state import Base, State
@@ -11,9 +17,13 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -23,3 +33,4 @@ if __name__ == '__main__':
         session.delete(state)
 
     session.commit()
+
