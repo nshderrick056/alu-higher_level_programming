c96a00b210e6e0f807b9abe60f248f8e7fb1e580
nshderrick056 <d.gatete@alustudent.com>
2025-08-01 14:03:38 +0200

    debugging model state

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# Script that takes in a URL, sends a request, and displays the size of the body in bytes
 curl -s "$1" | wc -c
